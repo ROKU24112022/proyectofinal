692580bd8df1934b37c633dc004e75b43258e621
RobinsonCuesta <apps2021colombia@gmail.com>
2022-06-30 19:15:00 -0500

    cargue de componentes

--- altair/src/app/app.module.ts
+++ altair/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RecoverComponent } from './componentes/recover/recover.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { PrincipalpageComponent } from './componentes/principalpage/principalpage.component';
+import { ConverterComponent } from './componentes/tools/converter/converter.component';
+import { DistanceComponent } from './componentes/tools/distance/distance.component';
+import { LocationComponent } from './componentes/tools/location/location.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RecoverComponent,
+    RegisterComponent,
+    PrincipalpageComponent,
+    ConverterComponent,
+    DistanceComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
