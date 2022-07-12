f4c27c465e58ac3d05022bf6524c61ce30e22e7f
JuanCarlosS0890 <106031177+JuanCarlosS0890@users.noreply.github.com>
2022-07-12 18:57:26 -0500

    ts_login_final

--- altair/src/app/componentes/login/login.component.ts
+++ altair/src/app/componentes/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { disableDebugTools } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,8 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+  usuarios:any = []
+  password:any = []
+  data:any [] = []
 
   ngOnInit(): void {
+    console.log('Binvenido a login')
+  }
+
+  iniciar(){
+    this.data.push(this.usuarios,this.password)
+    console.log(this.usuarios,this.password)
+  }
+  recuperar(){
+
   }
 
 }
