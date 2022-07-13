7d2d77affb3710e624a2adb69e61850a90a9bd53
JuanCarlosS0890 <106031177+JuanCarlosS0890@users.noreply.github.com>
2022-07-12 19:04:51 -0500

    rutas actualizadas_para_login_recover

--- altair/src/app/app-routing.module.ts
+++ altair/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
+import { RecoverComponent } from './componentes/recover/recover.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'recover',component:RecoverComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
